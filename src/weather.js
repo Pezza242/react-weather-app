2fe7768595967d9cbdebf1f25fd060b566078e8b
Pezza242 <periyearwood@gmail.com>
2026-01-04 15:55:52 +0000

    Created.weather.css

--- src/weather.js
+++ src/weather.js
@@ -3,7 +3,12 @@ import React from "react";
 export default function Weather() {
   return (
     <div className="weather">
-      <img src="https://i.ibb.co/qY9z3CRM/sun.png" alt="sun" border="0" />
+      <img
+        className="weather-icon"
+        src="https://i.ibb.co/qY9z3CRM/sun.png"
+        alt="sun"
+        border="0"
+      />
       <h1 className="city">London</h1>
       <h2 className="temperature">0º</h2>
     </div>
