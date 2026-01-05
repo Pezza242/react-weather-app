313f89689c7240806bd61cd40c16cc41a71728fc
Pezza242 <periyearwood@gmail.com>
2026-01-05 19:17:02 +0000

    Added search bar and submit button
    Also decorated search bar and button on css

--- src/weather.js
+++ src/weather.js
@@ -4,6 +4,16 @@ import "./weather.css";
 export default function Weather() {
   return (
     <div className="weather">
+      <form className="search-form">
+        <input
+          className="search-bar"
+          type="search"
+          placeholder="Enter a city..."
+          required
+        />
+        <input className="submit" type="submit" />
+      </form>
+      <hr />
       <img
         className="weather-icon"
         src="https://i.ibb.co/qY9z3CRM/sun.png"
